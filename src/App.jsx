11335c600f01c3755d6fb529d912377f18b62c1f
Cristina Navas <cris.30a@hotmail.com>
2024-04-03 21:55:53 -0500

    Paragraph 3

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <img src="src\assets\yoshi.jpg" alt="" />
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi libero saepe exercitationem earum alias recusandae repudiandae nulla! Blanditiis fuga tempora non numquam voluptates nihil explicabo assumenda nostrum alias? Delectus, officiis?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, aliquam nisi veritatis nobis quae dolores temporibus ducimus delectus eum earum voluptates consequuntur soluta? Deserunt fugit adipisci doloribus vero libero perferendis?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officiis, magnam itaque tenetur dolores nam officia alias velit dicta temporibus quos consequuntur reprehenderit tempore repellat eos explicabo facilis delectus sapiente.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
